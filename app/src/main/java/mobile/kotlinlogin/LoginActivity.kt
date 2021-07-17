43fcd4f459c0b0ef24fefecade69b845afbc6afa
zoran3 <zoran@mediportal.com>
2021-07-17 20:11:33 +0200

    Coroutines fix bug

--- app/src/main/java/mobile/kotlinlogin/LoginActivity.kt
+++ app/src/main/java/mobile/kotlinlogin/LoginActivity.kt
@@ -15,6 +15,8 @@ import mobile.kotlinlogin.rest.AppPreferences
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
+import java.util.*
+
 class LoginActivity : AppCompatActivity() {
     var dataList = ArrayList<LoginModel>()
     var uemail:String=""
